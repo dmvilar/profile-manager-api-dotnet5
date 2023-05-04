068ef14b20e0566ec9051cf4662e935bd97a5a9b
danie <danie@DELLG15>
2023-05-04 19:50:13 -0300

    Add project files.

---
+++ ProfileManager.Infrastructure/MongoDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using ProfileManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileManager.Infrastructure
+{
+    public class MongoDbContext : DbContext
+    {
+
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MongoConnection");
+            var databaseName = "Cluster0";
+
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<Profile> Profile => _database.GetCollection<Profile>("profile");
+
+    }
+}
---
+++ ProfileManager/Controllers/ProfileManagerController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProfileManager.Domain.Models;
+using ProfileManager.Domain.Interfaces;
+using System.Threading;
+
+namespace ProfileManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfileManagerController : ControllerBase
+    {
+        private readonly ILogger<ProfileManagerController> _logger;
+
+        private readonly IProfileRepository _profileRepository;
+
+        public ProfileManagerController(ILogger<ProfileManagerController> logger, IProfileRepository profileRepository)
+        {
+            _logger = logger;
+            _profileRepository = profileRepository;
+        }
+
+        [HttpPost("/CreateProfile")]
+        public async Task CreateProfile(Profile profile, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _profileRepository.CreateProfileAsync(profile, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet("/GetAll")]
+        public async Task<IEnumerable<Profile>> GetAll(CancellationToken cancellationToken)
+        {
+            return await _profileRepository.GetAllAsync(cancellationToken);
+        }
+
+        //[HttpGet]
+        //public Profile GetById()
+        //{
+        //    return new Profile();
+        //}
+
+        //[HttpGet]
+        //public IEnumerable<Profile> GetBySkill()
+        //{
+        //    return new[] { new Profile() };
+        //}
+
+    }
+}
